7bd930f12f076453ae4a08c69e6a48acbe93442f
WebZerefose <anything.codable@gmail.com>
2023-02-17 19:45:42 -0300

    Shopping Card Screen

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import {View} from 'react-native';
 import React from 'react';
 import ProductsScreen from './src/screens/ProductsScreen';
 import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
+import ShoppingCart from './src/screens/ShoppingCart';
 
 const App = () => {
   return (
     <View>
       {/* <ProductsScreen /> */}
-      <ProductDetailsScreen />
+      {/* <ProductDetailsScreen /> */}
+      <ShoppingCart />
     </View>
   );
 };
---
+++ src/screens/ShoppingCart.js
@@ -0,0 +1,86 @@
+import {FlatList, Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import cart from '../data/cart';
+import CartListItem from '../components/CartListItem';
+
+const ShoppingCartTotals = () => (
+  <View style={styles.totalsContainer}>
+    <View style={styles.row}>
+      <Text style={styles.text}>Subtotal</Text>
+      <Text style={styles.text}>$450,00</Text>
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.text}>Delivery</Text>
+      <Text style={styles.text}>$10,00</Text>
+    </View>
+    <View style={styles.row}>
+      <Text style={styles.textBold}>Total</Text>
+      <Text style={styles.textBold}>$460,00</Text>
+    </View>
+  </View>
+);
+
+const ShoppingCart = () => {
+  return (
+    <>
+      <FlatList
+        data={cart}
+        renderItem={({item}) => <CartListItem cartItem={item} />}
+        ListFooterComponent={ShoppingCartTotals}
+      />
+      <Pressable onPress={() => {}} style={styles.button}>
+        <Text style={styles.buttonText}>Check-out</Text>
+      </Pressable>
+    </>
+  );
+};
+
+export default ShoppingCart;
+
+const styles = StyleSheet.create({
+  totalsContainer: {
+    padding: 20,
+    borderTopColor: '#becbd6',
+    borderTopWidth: 0.4,
+    margin: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 2,
+  },
+  text: {
+    fontSize: 16,
+    color: '#687387',
+  },
+  textBold: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#88abb8',
+  },
+  button: {
+    position: 'absolute',
+    bottom: -40,
+    alignSelf: 'center',
+    backgroundColor: '#687387',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 50,
+    width: '90%',
+    borderRadius: 100,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '600',
+  },
+});
